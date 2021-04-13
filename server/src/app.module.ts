5d771a8ef261c8a89e2d8631545243622c68f43e
David Cedres <david.cedresx@gmail.com>
2021-04-13 03:53:57 -0400

    add periodic task and buggy docker-compose setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ScheduleModule.forRoot(), ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/articles/articles.service.ts
+++ server/src/articles/articles.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticlesService {
+  private readonly logger = new Logger(ArticlesService.name);
+
   create(createArticleDto: CreateArticleDto) {
     return 'This action adds a new article';
   }
@@ -23,4 +26,20 @@ export class ArticlesService {
   remove(id: number) {
     return `This action removes a #${id} article`;
   }
+
+  @Cron('45 * * * * *')
+  fetchNewArticles() {
+    this.logger.debug('Called when the current second is 45');
+
+    // fetch https://hn.algolia.com/api/v1/search_by_date?query=nodejs
+
+    // response.hits.
+    // title
+    // story_title
+    // story_utl
+    // url
+    // story_id
+
+  }
+
 }
